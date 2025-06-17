e5afa809224572b765b3c4de79d885163d6112f4
Zheheng <henryyzh0309@gmail.com>
2025-06-18 01:42:54 +0800

    fieldname modification

--- constant/fieldNames.js
+++ constant/fieldNames.js
@@ -2,8 +2,564 @@
 const crtuser = "Created By";
 const mntuser = "Last Modified By";
 
+//prfunacs
+const pracsfun = "Function Code";
+const pracsrol = "Role Code";
+const pracssts = "Access Status";
+const pracsa01 = "Action - 01";
+const pracsa02 = "Action - 02";
+const pracsa03 = "Action - 03";
+const pracsa04 = "Action - 04";
+const pracsa05 = "Action - 05";
+const pracsa06 = "Action - 06";
+const pracsa07 = "Action - 07";
+const pracsa08 = "Action - 08";
+const pracsa09 = "Action - 09";
+const pracsa10 = "Action - 10";
+
+//prfuncde
+const prfuncde = "Function Code";
+const prfunnme = "Function Name";
+const prfunlnm = "Function Local Name";
+const prfunsts = "Function Status";
+const prfungrp = "Function Group";
+
+const prfuna01 = "Action - 01";
+const prfuna02 = "Action - 02";
+const prfuna03 = "Action - 03";
+const prfuna04 = "Action - 04";
+const prfuna05 = "Action - 05";
+const prfuna06 = "Action - 06";
+const prfuna07 = "Action - 07";
+const prfuna08 = "Action - 08";
+const prfuna09 = "Action - 09";
+const prfuna10 = "Action - 10";
+
+const prfunl01 = "Label - 01";
+const prfunl02 = "Label - 02";
+const prfunl03 = "Label - 03";
+const prfunl04 = "Label - 04";
+const prfunl05 = "Label - 05";
+const prfunl06 = "Label - 06";
+const prfunl07 = "Label - 07";
+const prfunl08 = "Label - 08";
+const prfunl09 = "Label - 09";
+const prfunl10 = "Label - 10";
+
+//prgencde
+const prgtycde = "General Type";
+const prgecode = "General Code";
+const prgedesc = "General Code Description";
+const prgeldes = "General Code Local Description";
+
+//prgentyp
+const prgtydsc = "General Type Code Description";
+const prgtylds = "General Type Code Local Description";
+const prgtylen = "General Type Length";
+const prgtyman = "Mandatory (GTMAND: M-Mandatory, O-Optional)";
+const prgtycat = "Category (GTCAT: B - Business, T - Technical)";
+
+//prpwdpol
+const prpwdatm = "Failed Attempts to Lock Account";
+const pratmmsg = "Failed Attempts Error Message";
+const prpwdlen = "Password Length";
+const prlenmsg = "Password Length Error Message";
+const prpwdfrq = "Password Change Frequency";
+const prfrqmsg = "Change Password Frequency Error Message";
+const prpwdupc = "Password Require Upper Case Letter?";
+const prupcmsg = "Password Require Uppercase Letter Error Message";
+const prpwdlwc = "Password Require Lowercase Letter?";
+const prlwcmsg = "Password Require Lowercase Letter Message";
+const prpwdspc = "Password Require Special Character?";
+const prspcmsg = "Password Require Special Character Message";
+const prspcchr = "Password Allowed Special Characters";
+const prscrmsg = "Password Allowed Special Character Message";
+const prpwdnum = "Password Require Numeric Character";
+const prnummsg = "Password Require Numeric Character Message";
+const prcrtusr = "Creation User";
+const prmntusr = "Maintenance User";
+
+
+//psdocmas
+const psdocfnm = "Document File Name";
+const psdoconm = "Original File Name";
+const psdocudt = "Upload Date";
+const psdoctyp = "Document Path (1: temp_document, 2: store_front_image, 3: store_profile_picture_image)";
+const psdocext = "Document Extension";
+
+//psmbrprf
+const psmbruid = "Member ID";
+const psmbrnam = "Name";
+const psmbreml = "Email Address";
+const psmbrdob = "Date of Birth";
+const psmbrpts = "Member Point Balance";
+const psmbracs = "Member Accumulated Spending";
+const psmbrtyp = "Member Type";
+const psmbrexp = "Expired Date";
+const psmbrjdt = "Join Date";
+const psmbrcar = "Member Cart No";
+const psusrnme = "Username";
+const psmbrpre = "Phone Number Prefix";
+const psmbrphn = "Phone Number";
+
+//psmrclbl
+const psmrcuid = "Merchant ID";
+const psmrctyp = "Merchant Type";
+
+//psmrcpar
+const psmrcnme = "Merchant Name";
+const psmrcdsc = "Merchant Description";
+const psmrclds = "Merchant Local Description";
+const psmrcsdt = "Status Date";
+const psmrcjdt = "Join Date";
+const psmrcown = "Merchant Owner";
+const psmrcssm = "SSM No";
+const psmrcssc = "SSM Cert Image";
+const psmrcsts = "Merchant Status (YESORNO)";
+const psmrcbnk = "Merchant Bank";
+const psmrcacc = "Merchant Bank Account";
+const psmrcbnm = "Merchant Bank Name";
+const psmrcsfi = "Store Front Image";
+const psmrcppi = "Profile Picture Image";
+const psmrcrtg = "Rating";
+const psmrcrmk = "Remarks";
+
+//psorditm
+const psorduid = "Order Id";
+const psprduid = "Product Id";
+const psitmqty = "Order Item Quantity";
+const psitmrmk = "Order Item Remark";
+const psitmdsc = "Order Item Description";
+const psitmunt = "Unit Price";
+const psitmsbt = "Subtotal";
+const psitmcno = "Increment Number";
+
+
+//psordpar
+const psordodt = "Order Date";
+const psordamt = "Total Amount";
+const psordrap = "Reward Applied";
+const psrwduid = "Reward ID";
+const psordpap = "Point Applied";
+const psordpdv = "Point Discounted Value";
+const psordgra = "Grand Total";
+const psordpre = "Phone No Prefix";
+const psordphn = "Member Phone No";
+const psordsts = "Order Status";
+const psordocd = "Order Completed Date";
+const psordsst = "SST Amount";
+
+
+//psprddtl
+const psprdapn = "Add On Product Name";
+const psprdaty = "Add On Type";
+const psprdmnd = "Mandatory Indicator";
+const psprdpri = "Price";
+
+//psprdinv
+const psinvsty = "Stock Type (STKTYP)";
+const psinvqty = "Quantity";
+const psinvsdt = "Stock Date";
+
+//psprdpar
+const psprdnme = "Product Name";
+const psprddsc = "Product Description";
+const psprdlds = "Product Local Description";
+const psprdimg = "Product Image";
+const psprdtyp = "Product Type (PRODTYP)";
+const psprdcat = "Product Category (PRODCAT)";
+const psprdfvg = "For Vegetarian (YESORNO)";
+const psprdhal = "Is Halal (YESORNO)";
+const psprdsts = "Product Status (PRODSTS)";
+const psprdsdt = "Status Date";
+const psprdcid = "Countable Indicator (YESORNO)";
+const psprdlsr = "Low Stock Reminder";
+const psprdstk = "Stock";
+const psprdddt = "Discount Date";
+const psprddva = "Discount Value";
+const psprdcrd = "Creation Date";
+const psprdrmk = "Remarks";
+const psprdrtg = "Rating";
+
+//psrolpar  
+const psrolcde = "Role Code";
+const psroldsc = "Role Description";
+const psrollds = "Role Local Description";
+const psrolibi = "Interbranch Data Inquiry (YESORNO)";
+const psrolibm = "Interbranch Data Maintenance (YESORNO)";
+
+//psrwdpar
+const psrwdnme = "Reward Name";
+const psrwddsc = "Reward Description";
+const psrwdlds = "Reward Local Description";
+const psrwdfdt = "From Date";
+const psrwdtdt = "To Date";
+const psrwdtyp = "Discount Type (DISTYPE)";
+const psrwddva = "Discount Value";
+const psrwdism = "Min Indicator (YES/NO)";
+const psrwdmin = "Min Amount";
+const psrwdica = "Capped Indicator (YES/NO)";
+const psrwdcap = "Capped Amount";
+const psrwdaam = "Applicable to All Merchant (YES/NO)";
+const psrwdsts = "Reward Status";
+const psrwdqty = "Quantity";
+
+//psstfpar
+const psstfuid = "Staff Id";
+const psstfnme = "First Name";
+const psstftyp = "Staff Type";
+const psstfidt = "ID Type";
+const psstfidn = "ID No.";
+const psstfprp = "Profile Picture";
+const psstfnat = "Nationality";
+const psstfsdt = "Status Date";
+const psstfjdt = "Join Date";
+const psstfsts = "Staff Status";
+const psstfad1 = "Current Address Line 1";
+const psstfad2 = "Current Address Line 2";
+const psstfpos = "Current Postcode";
+const psstfcit = "Current City";
+const psstfsta = "Current State";
+const psstfchp = "CurrentHp";
+const psstfsam = "Same Address";
+const psstfha1 = "Home Address Line 1";
+const psstfha2 = "Home Address Line 2";
+const psstfhpo = "Home Postcode";
+const psstfhci = "Home City";
+const psstfhst = "Home State";
+const psstfeml = "Email Address";
+const psstfbnk = "Bank Name";
+const psstfacc = "Bank Account No";
+const psstfbnm = "Bank Account User Name";
+const psstfepr = "Emergency Contact Prefix";
+const psstfehp = "Emergency Contact";
+const psusrunm = "Username";
+
+//pssysann
+const psannuid = "Announcement Code";
+const psannttl = "Announcement Title";
+const psannmsg = "Announcement Message";
+const psanntyp = "Announcement Type";
+const psannsts = "Announcement Status";
+const psanndat = "Announcement Notified Date Time";
+const psannimg = "Image";
+
+//pssyspar
+const pscomnme = "Company Name";
+const pscomidn = "Company ID Number";
+const prpwdpol = "Password Policy Indicator (YESORNO)";
+const pslotime = "Auto Logout timer(Seconds)";
+
+//pstblkey
+const pstblnme = "Table Name";
+const pstblkyn = "Table Key Name";
+const pstblkys = "Table Key Sequence Number";
+
+//pstblmas
+const pstbltyp = "Table Type";
+const pstbldsc = "Table Description";
+const pstbllds = "Table Local Description";
+const pstblpnt = "Table Parent";
+
+//psusrprf
+const psusrnam = "Name";
+const psusreml = "Email Address";
+const psusrpwd = "Password";
+const psusrsts = "Account Status - General Code (USRSTS, A - Active, L - Locked , C - Closed, E-Expired)";
+const psusrtyp = "User Type - General Code (USRTYP, ADM - Admin, MBR - Member, MCH - Merchant)";
+const psusrphn = "HP Number";
+const psusrpre = "HP Prefix (HPPRE)";
+const psstsdt8 = "Status Change Date";
+const pspwdatm = "Password Attempts";
+const pschgpwd = "Change Password Flag";
+const psfstlgn = "First Login Flag";
+const psusrrol = "User Role - General Code (USRROLE)";
+const psmsgurd = "Unread Messages";
+
+//usrlgnpf
+const pslgnsts = "Login Status";
+const pslgidat = "Login Time";
+const pslgodat = "Logout Time";
+const pslgntkn = "Login Token";
+
+
 module.exports = {
+    // Misc
     crtuser,
     mntuser,
 
-}
+    // prfunacs
+    pracsfun,
+    pracsrol,
+    pracssts,
+    pracsa01,
+    pracsa02,
+    pracsa03,
+    pracsa04,
+    pracsa05,
+    pracsa06,
+    pracsa07,
+    pracsa08,
+    pracsa09,
+    pracsa10,
+
+    // prfuncde
+    prfuncde,
+    prfunnme,
+    prfunlnm,
+    prfunsts,
+    prfungrp,
+    prfuna01,
+    prfuna02,
+    prfuna03,
+    prfuna04,
+    prfuna05,
+    prfuna06,
+    prfuna07,
+    prfuna08,
+    prfuna09,
+    prfuna10,
+    prfunl01,
+    prfunl02,
+    prfunl03,
+    prfunl04,
+    prfunl05,
+    prfunl06,
+    prfunl07,
+    prfunl08,
+    prfunl09,
+    prfunl10,
+
+    // prgencde
+    prgtycde,
+    prgecode,
+    prgedesc,
+    prgeldes,
+
+    // prgentyp
+    prgtydsc,
+    prgtylds,
+    prgtylen,
+    prgtyman,
+    prgtycat,
+
+    // prpwdpol
+    prpwdatm,
+    pratmmsg,
+    prpwdlen,
+    prlenmsg,
+    prpwdfrq,
+    prfrqmsg,
+    prpwdupc,
+    prupcmsg,
+    prpwdlwc,
+    prlwcmsg,
+    prpwdspc,
+    prspcmsg,
+    prspcchr,
+    prscrmsg,
+    prpwdnum,
+    prnummsg,
+    prcrtusr,
+    prmntusr,
+
+    // psdocmas
+    psdocfnm,
+    psdoconm,
+    psdocudt,
+    psdoctyp,
+    psdocext,
+
+    // psmbrprf
+    psmbruid,
+    psmbrnam,
+    psmbreml,
+    psmbrdob,
+    psmbrpts,
+    psmbracs,
+    psmbrtyp,
+    psmbrexp,
+    psmbrjdt,
+    psmbrcar,
+    psusrnme,
+    psmbrpre,
+    psmbrphn,
+
+    // psmrclbl
+    psmrcuid,
+    psmrctyp,
+
+    // psmrcpar
+    psmrcnme,
+    psmrcdsc,
+    psmrclds,
+    psmrcsdt,
+    psmrcjdt,
+    psmrcown,
+    psmrcssm,
+    psmrcssc,
+    psmrcsts,
+    psmrcbnk,
+    psmrcacc,
+    psmrcbnm,
+    psmrcsfi,
+    psmrcppi,
+    psmrcrtg,
+    psmrcrmk,
+
+    // psorditm
+    psorduid,
+    psprduid,
+    psitmqty,
+    psitmrmk,
+    psitmdsc,
+    psitmunt,
+    psitmsbt,
+    psitmcno,
+
+    // psordpar
+    psordodt,
+    psordamt,
+    psordrap,
+    psrwduid,
+    psordpap,
+    psordpdv,
+    psordgra,
+    psordpre,
+    psordphn,
+    psordsts,
+    psordocd,
+    psordsst,
+
+    // psprddtl
+    psprdapn,
+    psprdaty,
+    psprdmnd,
+    psprdpri,
+
+    // psprdinv
+    psinvsty,
+    psinvqty,
+    psinvsdt,
+
+    // psprdpar
+    psprdnme,
+    psprddsc,
+    psprdlds,
+    psprdimg,
+    psprdtyp,
+    psprdcat,
+    psprdfvg,
+    psprdhal,
+    psprdsts,
+    psprdsdt,
+    psprdcid,
+    psprdlsr,
+    psprdstk,
+    psprdpri,
+    psprdddt,
+    psprddva,
+    psprdcrd,
+    psprdrmk,
+    psprdrtg,
+
+    // psrolpar
+    psrolcde,
+    psroldsc,
+    psrollds,
+    psrolibi,
+    psrolibm,
+
+    // psrwdpar
+    psrwdnme,
+    psrwddsc,
+    psrwdlds,
+    psrwdfdt,
+    psrwdtdt,
+    psrwdtyp,
+    psrwddva,
+    psrwdism,
+    psrwdmin,
+    psrwdica,
+    psrwdcap,
+    psrwdaam,
+    psrwdsts,
+    psrwdqty,
+
+    // psstfpar
+    psstfuid,
+    psstfnme,
+    psstftyp,
+    psstfidt,
+    psstfidn,
+    psstfprp,
+    psstfnat,
+    psstfsdt,
+    psstfjdt,
+    psstfsts,
+    psstfad1,
+    psstfad2,
+    psstfpos,
+    psstfcit,
+    psstfsta,
+    psstfchp,
+    psstfsam,
+    psstfha1,
+    psstfha2,
+    psstfhpo,
+    psstfhci,
+    psstfhst,
+    psstfeml,
+    psstfbnk,
+    psstfacc,
+    psstfbnm,
+    psstfepr,
+    psstfehp,
+    psusrunm,
+
+    // pssysann
+    psannuid,
+    psannttl,
+    psannmsg,
+    psanntyp,
+    psannsts,
+    psanndat,
+    psannimg,
+
+    // pssyspar
+    pscomnme,
+    pscomidn,
+    pslotime,
+
+    // pstblkey
+    pstblnme,
+    pstblkyn,
+    pstblkys,
+
+    // pstblmas
+    pstbltyp,
+    pstbldsc,
+    pstbllds,
+    pstblpnt,
+
+    // psusrprf
+    psusrnam,
+    psusreml,
+    psusrpwd,
+    psusrsts,
+    psusrtyp,
+    psusrphn,
+    psusrpre,
+    psstsdt8,
+    pspwdatm,
+    pschgpwd,
+    psfstlgn,
+    psusrrol,
+    psmsgurd,
+
+    // usrlgnpf
+    pslgnsts,
+    pslgidat,
+    pslgodat,
+    pslgntkn
+};
